7369e6be23a470ac720ea1f7d75a7836284fd921
enyu0510 <jonathanchen0510@gmail.com>
2021-10-05 23:48:46 -0400

    Implement start/pause button

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
-  const ScoreDisplay = document.querySelector("#score");
-  const StartBtn = document.querySelector("#start-button");
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
   const width = 10;
   let nextRandom = 0;
+  let timerId;
 
   //Tetrominoes
   const lTetromino = [
@@ -70,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //make tetromino move down every second
-  timerId = setInterval(moveDown, 1000);
+  // timerId = setInterval(moveDown, 1000);
 
   //assign functions to keyCodes
   function control(e) {
@@ -186,4 +187,17 @@ document.addEventListener("DOMContentLoaded", () => {
       displaySquares[displayIndex + index].classList.add("tetromino");
     });
   }
+
+  //add functionality to button
+  startBtn.addEventListener("click", () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      displayShape();
+    }
+  });
 });
