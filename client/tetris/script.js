2e351ebbceb7f30f3b1ecdfc333992dbbdc4cf82
enyu0510 <jonathanchen0510@gmail.com>
2021-10-05 22:14:43 -0400

    Add arrow key controls

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -71,6 +71,20 @@ document.addEventListener("DOMContentLoaded", () => {
   //make tetromino move down every second
   timerId = setInterval(moveDown, 1000);
 
+  //assign functions to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      //rotate
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener("keyup", control);
+
   //move down
   function moveDown() {
     undraw();
@@ -95,5 +109,41 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
     }
+  }
+
+  //move tetromino left unless at edge or blockage
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some(
+        (index) => squares[currentPosition + index].classList.contains["taken"]
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  //move tetromino right, unless at edge or blockage
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+    if (!isAtRightEdge) currentPosition += 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
   }
 });
