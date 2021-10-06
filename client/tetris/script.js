10bead398f79337cae2fd7c5544a66cc8cc60b7e
enyu0510 <jonathanchen0510@gmail.com>
2021-10-05 22:35:06 -0400

    Add rotation capability

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //rotate
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -144,6 +144,18 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       currentPosition -= 1;
     }
+    draw();
+  }
+
+  //rotate tetromino
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      //if current rotation gets to 4, go back to 0
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
     draw();
   }
 });
