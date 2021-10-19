efd37202646c7e439c6268a72b9aefad2ff29e2a
enyu0510 <jonathanchen0510@gmail.com>
2021-10-19 12:31:39 -0400

    Add pan and empty string event listener

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,23 +1,23 @@
 function mapInit() {
-  const mymap = L.map("mapid").setView([38.989, -76.937], 11);
+  const mymap = L.map('mapid').setView([38.989, -76.937], 11);
   L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
     {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: "mapbox/streets-v11",
+      id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken:
-        "pk.eyJ1IjoiZW55dTA1MTAiLCJhIjoiY2t1dWppNTVtNXpjdDJxbnppcDVqcTNjeiJ9.eONbPxY7fFgd1PTsKlWz2A",
+        'pk.eyJ1IjoiZW55dTA1MTAiLCJhIjoiY2t1dWppNTVtNXpjdDJxbnppcDVqcTNjeiJ9.eONbPxY7fFgd1PTsKlWz2A'
     }
   ).addTo(mymap);
-  return mymap;
+  return mymap; 
 }
 
 async function dataHandler() {
-  endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+  endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   const request = await fetch(endpoint);
   const restaurants = await request.json();
   const mymap = mapInit();
@@ -25,7 +25,7 @@ async function dataHandler() {
   function findMatches(wordToMatch, restaurants) {
     return restaurants.filter((place) => {
       // figure out if search matches
-      const regex = new RegExp(wordToMatch, "gi");
+      const regex = new RegExp(wordToMatch, 'gi');
       // filter by zip code
       return place.zip.match(regex);
     });
@@ -45,7 +45,8 @@ async function dataHandler() {
       const marker = latLong.reverse();
       L.marker(marker).addTo(mymap);
     });
-
+    // pan map to first restaurant
+    mymap.setView(limitedList[0].geocoded_column_1.coordinates, 11);
     const html = limitedList
       .map(
         (place) => `  
@@ -56,16 +57,21 @@ async function dataHandler() {
     <br>
         `
       )
-      .join("");
+      .join('');
 
     suggestions.innerHTML = html;
   }
 
-  const searchInput = document.querySelector(".search");
-  const suggestions = document.querySelector(".suggestions");
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
 
   // searchInput.addEventListener("change", displayMatches); no longer needed
-  searchInput.addEventListener("input", (evt) => {
+  searchInput.addEventListener('input', (evt) => {
+    if (!searchInput) {
+      limitedList = '';
+    }
+  });
+  searchInput.addEventListener('input', (evt) => {
     displayMatches(evt);
   });
 }
